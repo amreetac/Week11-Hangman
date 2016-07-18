dd938a2c260305a21a9580c8ea10ded53ef5d209
Amreeta Choudhury <amreeta89@yahoo.com>
2016-07-17 23:08:53 -0400

    Moved game functionality to main.js and kept original game.js code

--- game.js
+++ game.js
@@ -1,112 +1,11 @@
-/*
-
-Initial Brainstorming
-Object: 
-	game
-Attributes:
-	wordList // list of words to choose from 
-	guessesAllow // number of guesses allowed
-	guessesSoFar // number of guesses taken for this game
-	wrongguesses // string of letters guessed wrong. If a person guesses right, it doesn't penalize, so I'm only counting wrong guesses
-	word 	// word object to handle guesses and updates of the
-			// word to be displayed
-Methods:
-	game(guessesAllowed) 	// Constructor to init attributes. 
-							// Set current Word to a random word
-							// in the word list
-	play() 	// Play this game until the player uses up the number of 
-			// guesses allowed
-	takeAGuess() // Prompt User for a new letter to guess.  Call
-				 // word.guessAletter() with the letter entered
-				 // Returns true if found, otherwise false
-*/
-
-debugOn = false; //For debugging later on
-
-var inquirer = require('inquirer');
-var word = require('./word.js');
-
-function game(guessesAllow) {
-	//attributes
-	this.guessesAllow = guessesAllow;
-	this.guessesSoFar = "";
-	this.wrongGuesses = 0;
-	this.Correct = 0;
-
-	this.wordlist = ["javascript", "nodejs", "mysql", "mongodb", 
-		"webdesign", "coder"];
-
-	var random = Math.floor((Math.random()*(this.wordlist.length-1)));
-
-	this.word = new word(this.wordlist[random]); 
-
-	//methods
-	this.play = function() { 
-		//console.log("NEW GAME!"); Done for testing purposes.
-
-		var self = this;
-
-    //RUNS INQUIRER AND ASKS THE USER A SERIES OF QUESTIONS WHOSE REPLIES ARE STORED WITHIN 
-    //THE VARIABLE answers INSIDE OF THE .then STATEMENT.
-    inquirer.prompt([{
-    type: 'input',
-    message: 'Hello, what is your guess?',
-    name: 'letter'
-    }]).then(function(input) {
-
-    	if (debugOn == true) {
-    		console.log("Then function from prompt");
-    	}
+/* This was my original game.js */
 
 
-    	// Check the index of the letter, if it is correct then play. 
-  		if (self.guessesSoFar.indexOf(input.letter) >=0) {
-  			self.play();
-  		//console.log("letter1");
-  			return;
-  		}
-  		//console.log("letter2");
-  		self.guessesSoFar += input.letter;
+var wordlist = [
+    "javascript", "nodejs", "mysql", "mongodb", "webdesign", "coder"
+]
+var random = Math.floor((Math.random()*(wordlist.length-1)));
 
+var word = wordlist[random];
 
-  		var count= self.word.hasLetter(input.letter); //# times it found the letter
-
-  		//self.Correct += count; 
-
-  		self.word.display();
-
-  		if (count > 0) {
-  			self.Correct += count;
-  		} else {
-  			self.wrongGuesses++;
-  		}
-
-  		if (self.Correct == self.word.length) {
-  			console.log ("You have won the game!");
-  		}
-  		else if (self.wrongGuesses < self.guessesAllow) {
-  				self.play();
-  			}
-  		else {
-  			console.log("You lost!");
-  		}
-        		
-    })
-
-}
-
-	this.log = function() {
-		console.log("game.guessesAllow: " + this.guessesAllow);
-		console.log("game.guessesSoFar: " + this.guessesSoFar);
-		console.log("game.wrongGuesses: "+ this.wrongGuesses);
-
-		this.word.log();
-	}
-}
-
-
-
-module.exports = game;
-var g = new game(10);
-//g.log();
-g.play();
+console.log(word);
--- main.js
+++ main.js
@@ -1,52 +1,54 @@
 /*
 
-Here are various fragments of code I have tried to run on main at different times.
-As of now, the game works from game.js
+Initial Brainstorming
+Object: 
+	game
+Attributes:
+	wordList // list of words to choose from 
+	guessesAllow // number of guesses allowed
+	guessesSoFar // number of guesses taken for this game
+	wrongguesses // string of letters guessed wrong. If a person guesses right, it doesn't penalize, so I'm only counting wrong guesses
+	word 	// word object to handle guesses and updates of the
+			// word to be displayed
+Methods:
+	game(guessesAllowed) 	// Constructor to init attributes. 
+							// Set current Word to a random word
+							// in the word list
+	play() 	// Play this game until the player uses up the number of 
+			// guesses allowed
+	takeAGuess() // Prompt User for a new letter to guess.  Call
+				 // word.guessAletter() with the letter entered
+				 // Returns true if found, otherwise false
 */
 
-/*
+debugOn = false; //For debugging later on
 
-var game = require('./game.js');
-var letter = require('./letter.js');
+var inquirer = require('inquirer');
 var word = require('./word.js');
 
+function game(guessesAllow) {
+	//attributes
+	this.guessesAllow = guessesAllow;
+	this.guessesSoFar = "";
+	this.wrongGuesses = 0;
+	this.Correct = 0;
 
+	this.wordlist = ["javascript", "nodejs", "mysql", "mongodb", 
+		"webdesign", "coder"];
 
+	var random = Math.floor((Math.random()*(this.wordlist.length-1)));
 
-/* Brainstorming of game 
+	this.word = new word(this.wordlist[random]); 
 
- Objects: main, game, word, letter
+	//methods
+	this.play = function() { 
+		//console.log("NEW GAME!"); Done for testing purposes.
 
-Object: 
-	main
+		var self = this;
 
-Attributes:
-	maxGames // total number of games to play
-	numWins // total number wins
-	prompt // use inquirer.js
-Methods:
-	welcomePrompt() // Welcome to hangman, show options:
-					// New Game, Get Stats, Quit
-	start()	// Called When New Game is selected, will create a
-			// new game, initialize it, and play until win/loss
-	getStats() // Display the current total wins/losses of all the games played.
-	quit() // Display the final stats, leave the game.
-*/
-/*
-function main ()
-{
-
-this.game = new game(10);  Meant to run this for 10 times.
-
-}
-
-
-main();
-
-*/
-
-/*
-inquirer.prompt([{
+    //RUNS INQUIRER AND ASKS THE USER A SERIES OF QUESTIONS WHOSE REPLIES ARE STORED WITHIN 
+    //THE VARIABLE answers INSIDE OF THE .then STATEMENT.
+    inquirer.prompt([{
     type: 'input',
     message: 'Hello, what is your guess?',
     name: 'letter'
@@ -56,6 +58,55 @@ inquirer.prompt([{
     		console.log("Then function from prompt");
     	}
 
+
+    	// Check the index of the letter, if it is correct then play. 
+  		if (self.guessesSoFar.indexOf(input.letter) >=0) {
+  			self.play();
+  		//console.log("letter1");
+  			return;
+  		}
+  		//console.log("letter2");
+  		self.guessesSoFar += input.letter;
+
+
+  		var count= self.word.hasLetter(input.letter); //# times it found the letter
+
+  		//self.Correct += count; 
+
+  		self.word.display();
+
+  		if (count > 0) {
+  			self.Correct += count;
+  		} else {
+  			self.wrongGuesses++;
+  		}
+
+  		if (self.Correct == self.word.length) {
+  			console.log ("You have won the game!");
+  		}
+  		else if (self.wrongGuesses < self.guessesAllow) {
+  				self.play();
+  			}
+  		else {
+  			console.log("You lost!");
+  		}
+        		
+    })
+
 }
 
-*/
+	this.log = function() {
+		console.log("game.guessesAllow: " + this.guessesAllow);
+		console.log("game.guessesSoFar: " + this.guessesSoFar);
+		console.log("game.wrongGuesses: "+ this.wrongGuesses);
+
+		this.word.log();
+	}
+}
+
+
+
+module.exports = game;
+var g = new game(10);
+//g.log();
+g.play();
